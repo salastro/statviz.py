7c6e5a3401df6be33a64e20c406f5e528fbd0409
salastro <salah2112004@gmail.com>
2024-12-21 21:34:13 +0200

    Implement file open

--- gui/main.py
+++ gui/main.py
@@ -1,12 +1,19 @@
 import sys
-from PyQt6.QtWidgets import QApplication, QMainWindow
+
+from PyQt6.QtWidgets import QApplication, QFileDialog, QMainWindow
+
 from gui import Ui_MainWindow
+# from ..src import single_random_variable, joint_random_variable, function_of_random_variable, test_generator
+
 
 class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
         self.setup_connections()
+        self.file_path: str = ""
+        self.results: str = ""
+        self.analysis_mode: str = "Single Random Variable"
 
     def setup_connections(self):
         # Connect buttons and other UI elements to their respective slots
@@ -15,16 +22,29 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.SaveResultsBt.clicked.connect(self.save_results)
 
     def open_file(self):
-        # Logic for opening a file
-        print("Open file button clicked")
+        """
+        Open a file dialog to select samples .mat file
+        """
+        file_dialog = QFileDialog(self)
+        file_dialog.setFileMode(QFileDialog.FileMode.ExistingFile)
+        file_dialog.setNameFilter("MAT files (*.mat)")
+        file_dialog.setViewMode(QFileDialog.ViewMode.Detail)
+        file_dialog.setAcceptMode(QFileDialog.AcceptMode.AcceptOpen)
+        if file_dialog.exec():
+            selected_files = file_dialog.selectedFiles()[0]
+            self.file_path = selected_files
+            print(f"Selected file: {self.file_path}")
+        else:
+            print("No file selected")
 
     def analyze(self):
-        # Logic for performing analysis
+        # Logic for analyzing the file
         print("Analyze button clicked")
 
     def save_results(self):
         # Logic for saving results
         print("Save results button clicked")
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
